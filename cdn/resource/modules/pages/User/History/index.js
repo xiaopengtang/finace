8565db43da4905e2756c2bfc56d8f4ca500fba70
leexm0101 <853232271@qq.com>
2017-11-15 14:55:01 +0800

    all

---
+++ cdn/resource/modules/pages/User/History/index.js
@@ -0,0 +1,54 @@
+// 历史
+import React, {Component} from 'react'
+import User from '../../../layout/user'
+import { NoticeBar, WingBlank, Card, WhiteSpace, Button, List, Icon, Checkbox, Steps, Flex, Accordion, InputItem, Picke, Toast} from 'antd-mobile'
+const {Item} = List;
+const {Step} = Steps;
+
+const history = []
+
+export default class Index extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  state = {
+    history: [],
+  };
+
+	render () {
+		return (
+		    <User title="投资历史" module="userDetail" className="user-detail"
+		    	footer={
+            <div></div>
+				  }
+        >
+          <WingBlank>
+            {
+              history.length > 0
+              ? history.map((item)=>{
+                  return (
+                    <div className="J_historyItem">
+                      <Card>
+                        <Card.Body>
+                          <div>This is content of `Card`</div>
+                          </Card.Body>
+                      </Card>
+                    </div>
+                  )
+                })
+              : <div className="J_historyItem">
+                  <Card>
+                    <Card.Body>
+                      <div className="J_msgText">暂时没有投资历史!!!</div>
+                    </Card.Body>
+                  </Card>
+                </div>
+            }
+          </WingBlank>
+
+		    </User>
+		 )
+	}
+}
---
+++ cdn/resource/modules/pages/User/Recharge/index.js
@@ -0,0 +1,57 @@
+// 充值
+import React, {Component} from 'react'
+import User from '../../../layout/user'
+import { NoticeBar, WingBlank, Card, WhiteSpace, Button, List, Icon, Checkbox, Steps, Flex, Accordion, InputItem, Picker, Toast} from 'antd-mobile'
+const {Item} = List;
+const {Step} = Steps;
+
+
+export default class Index extends Component {
+
+  constructor(props){
+    super(props);
+    this.onChange = this.onChange.bind(this);
+  }
+
+  state = {
+    value: 0,
+  };
+
+  onChange(e){
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  // 确认按钮统一甩锅 @丹君
+  determine(){
+    console.log(1111);
+    Toast.loading("请稍后", 2, (e)=>{
+      Toast.offline('服务器端网络异常!!!', 1);
+    })
+  }
+
+	render () {
+		return (
+		    <User title="充值" module="userDetail" className="user-detail"
+		    	footer={
+            <div></div>
+				  }
+        >
+          <div>
+            <List renderHeader={() => '请输入充值金额:'} className="my-list">
+              <Item>
+                <input type="number" value={this.state.value} onChange={this.onChange}/>
+              </Item>
+            </List>
+          </div>
+
+          <WingBlank>
+            <div className="J_logout">
+              <Button type="primary" onClick={this.determine}>确认</Button>
+            </div>
+          </WingBlank>
+		    </User>
+		 )
+	}
+}
