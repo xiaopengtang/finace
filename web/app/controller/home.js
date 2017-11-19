bdb788c6a2f476bfb9298b03275e88ca5c882c00
唐孝朋 tangxiaopeng () <tangxiaopeng@tuniu.com>
2017-11-19 20:18:33 +0800

    修改

--- web/app/controller/home.js
+++ web/app/controller/home.js
@@ -1,10 +1,10 @@
 'use strict';
-// const render = require('../../resource/component/main')
+const render = require('../../resource/component/main')
 
 module.exports = app => {
   class HomeController extends app.Controller {
     * index(ctx) {
-    	const html = '' //yield render.default(ctx.url)
+    	const html = yield render.default(ctx.url)
     	const user = ctx.session.user || null
       return yield ctx.render('home/index.tpl', {user, html})
     }
